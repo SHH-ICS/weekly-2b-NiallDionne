f6f28a0495924cd29173c5c3284618e4c73bf181
NiallDionne <144242112+NiallDionne@users.noreply.github.com>
2023-11-03 08:42:32 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,6 @@
-
+import math 
+radius = float(input("What's Your Radius? "))
+area = math.pi*(radius**2)
+circumference = 2*(math.pi*radius)
+if radius > 0:
+    print(f"Your Circumference is {circumference} Your Area is {area}")
